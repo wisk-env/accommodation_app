2067cf160b0c32f25673c50f290d11ccdcfc987f
kfor-code <wiskenv566@gmail.com>
2023-05-09 23:54:59 +0900

    ユーザーの名前と自己紹介のカラムを追加

---
+++ db/migrate/20230509142945_add_name_and_introduction_to_user.rb
@@ -0,0 +1,6 @@
+class AddNameAndIntroductionToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :introduction, :string
+  end
+end
