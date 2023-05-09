528dc3f3c8590bb218143721e3f3070d65880326
kfor-code <wiskenv566@gmail.com>
2023-05-10 00:20:15 +0900

    ユーザーのプロフィールページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
   end
+
+  def profile_show
+    @user = User.find(current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get 'users/profile', to: 'users#profile_show'
+
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
