ef52e29edb2a5f610e27906c2f08e5533048048a
kfor-code <wiskenv566@gmail.com>
2023-05-10 22:27:23 +0900

    ユーザープロフィール編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def profile_show
     @user = User.find(current_user.id)
   end
+
+  def profile_edit
+    @user = User.find(current_user.id)
+  end
+
+  def profile_update
+    @user = User.find(current_user.id)
+    if @user.update(params.require(:user).permit(:name, :introduction))
+      flash[:notice] = "ユーザーのプロフィールが更新されました"
+      redirect_to :users_profile
+    else
+      render "profile_edit"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
   get 'users/profile', to: 'users#profile_show'
+  get 'users/profile/edit', to: 'users#profile_edit'
+  put 'user/profile', to: 'users#profile_update'
 
   devise_for :users, controllers: {
     sessions: "users/sessions",
