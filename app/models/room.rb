339a210d7f37dd8a4fc6b54e744918f0392c076d
kfor-code <wiskenv566@gmail.com>
2023-05-11 00:08:04 +0900

    Roomモデルカラムにバリデーション設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  validates :room_name, presence: true
+  validates :room_info, presence: true
+  validates :fee, presence: true, numericality: true
+  validates :address, presence: true
+  
 end
