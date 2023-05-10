7365a45d9c4882ef3729f94a270e5eb79b6bcaa1
kfor-code <wiskenv566@gmail.com>
2023-05-11 05:11:13 +0900

    施設画像登録機能追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,7 +17,7 @@ class RoomsController < ApplicationController
     @room = Room.new(params.require(:room).permit(:room_name, :room_info, :fee, :address, :user_id, :room_image))
     if @room.save
       flash[:notice] = "施設が作成されました"
-      redirect_to :rooms
+      redirect_to room_path(@room)
     else
       render "new"
     end
