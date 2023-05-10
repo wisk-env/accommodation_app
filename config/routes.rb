2bda553e75e335e2c67d187fcfe9784bdd2729e0
kfor-code <wiskenv566@gmail.com>
2023-05-11 00:02:51 +0900

    roomsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'rooms/show'
-  get 'rooms/new'
+  #get 'rooms/index'
+  #get 'rooms/show'
+  #get 'rooms/new'
   get 'users/account', to: 'users#show'
   get 'users/profile', to: 'users#profile_show'
   get 'users/profile/edit', to: 'users#profile_edit'
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+
+  resources :rooms, only: [:index, :show, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
