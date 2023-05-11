4bbfc66546381cace89948ec0f53f182aa7245c1
kfor-code <wiskenv566@gmail.com>
2023-05-11 21:16:58 +0900

    Reservationモデルのカラムにバリデーション設定

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :number_of_guests, presence: true, numericality: true
+
   belongs_to :user
   belongs_to :room
 end
